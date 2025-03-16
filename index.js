bae71ab3bb3b30637b88eb4eb05004e3b49afda7
ashrafulhossain1 <ahmeadashraful@gmail.com>
2025-03-17 02:36:57 +0600

    winter is start so pc will off

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
 const dotenv = require('dotenv');
@@ -7,3 +8,11 @@ dotenv.config();
 
 // mongoose
 // K4e5btpEUeiECxSf
+
+// connect to db
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log("Connection Failed",err));
+
+app.use(express.json());
+app.use('/api/users', require(userRoute));
